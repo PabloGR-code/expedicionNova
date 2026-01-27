82ff6d5379528af2c31f91db5e991d1a8a25ef11
Pablo <pagira@alumnatflorida.es>
2026-01-27 12:51:35 +0100

    añadiendo gestor entidad

--- models/GestorEntidad.php
+++ models/GestorEntidad.php
@@ -1,6 +1,32 @@
 <?php
+    
+    session_start();
 
     class GestorEntidad implements iGestor {
 
+        public function __construct(){
+            if(!isset($_SESSION['entidad'])){
+                $_SESSION['entidad']=[];
+            }
+        }
+
+        public function obtenerTodos(){
+            return $_SESSION['entidad'];
+        }
+
+        public function guardar($entidad){
+            $_SESSION['entidad'][]=$entidad;
+        }
+
+        public function eliminar($id){
+            foreach ($_SESSION['entidad'] as $i => $entidad) {
+                if($entidad->getId()== $id){
+                    unset($_SESSION['entidad'][$i]);
+                    $_SESSION['entidad']=array_values($_SESSION['entidad'])
+                    return true;
+                }
+            }
+            return false;
+        }
 
     }
