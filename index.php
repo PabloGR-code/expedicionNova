d7eeff383d4c78c887bdc201b8f1fd54152b8b2b
Pablo <pagira@alumnatflorida.es>
2026-01-30 08:58:39 +0100

    añadir index

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+require_once "autoload.php";
+session_start();
+
+$controller = new controller();
+
+$accion = $_GET['accion'] ?? 'index';
+
+switch ($accion) {
+    case 'obtenerTodos':
+        $controller->obetenerTodos();
+    case 'guardar':
+        $controller->guardar();
+        break;
+    case 'eliminar':
+        $controller->eliminar();
+        break;
+    case 'buscar':
+        $controller->buscar();
+        break;
+    case 'editar':
+        $controller->editar();
+        break;
+    default:
+        $controller->index();
+}
