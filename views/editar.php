14cfa4aba3b462d2e66aa863aa62ae2d9437e1cf
Pablo <pagira@alumnatflorida.es>
2026-02-02 09:19:51 +0100

    añadiendo editar

---
+++ views/editar.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Editar Expediente</title>
+</head>
+<body>
+    <h1>Editar Expediente</h1>
+
+    <form method="POST">
+        Nombre:<br>
+        <input type="text" name="nombre" required><br><br>
+
+        Planeta de origen:<br>
+        <input type="text" name="planetadeorigen" required><br><br>
+
+        Nivel de Estabilidad (0-10):<br>
+        <input type="number" name="nivel de estabilidad" required><br><br>
+
+        <button type="submit">Actualizar</button>
+    </form>
+
+    <br>
+    <a href="index.php">Volver</a>
+</body>
+</html>
